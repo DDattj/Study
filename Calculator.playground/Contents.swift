eaccab28c3e98ce5cff50ecc93b0701c8212dd1f
DDattj <20191886@sungshin.ac.kr>
2024-03-07 17:34:08 +0900

    2단계 수정 3단계 완료

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/*
 //일단 클래스 생성, 이름은 Calculator
 class Calculator {
     
@@ -30,33 +31,120 @@ class Calculator {
         return number1 * number2
     }
     
-    //답을 2개 추출하고 싶을땐 앞에 string 붙여서 문자열로 인식하게
-    // 그리고 꼭 \(답) 이거 해주기
+    //number가 0일때 오류가 날 경우도 작성해야한다!!!!
     func divide() -> Int {
-        return number1 / number2
-    } //2가 0일때 오류가 날 경우의 로그도 작성해야한다!!!! (((내일 수정하기
-    
-    func remaind() -> Int {
-            return number1 % number2
+        if number1 != 0 && number2 != 0 {
+            return number1 / number2
+        } else {
+            return 0
         }
     }
+    
+    func remaind() -> Int {
+        if number1 != 0 && number2 != 0 {
+            return number1 % number2
+        } else {
+            return 0
+        }
+    }
+}
 
 
     
 //실행해보기
 let calculator = Calculator(n1: 6, n2: 0)
-print(calculator.add())
-print(calculator.subtract())
-print(calculator.multiply())
-print(calculator.divide())
-print(calculator.remaind())
-
+print("더하면 \(calculator.add())")
+print("빼면 \(calculator.subtract())")
+print("곱하면 \(calculator.multiply())")
+print("나누면 \(calculator.divide())")
+print("나머지는 \(calculator.remaind())")
+//print 내에서 줄바꿈 하고싶을땐 print("\n")공백을 프린트 해주면 된다
+print("\n")
 let calculator2 = Calculator(n1: 8, n2: 3)
-print(calculator2.add())
-print(calculator2.subtract())
-print(calculator2.multiply())
-print(calculator2.divide())
+print("더하면 \(calculator2.add())")
+print("빼면 \(calculator2.subtract())")
+print("곱하면 \(calculator2.multiply())")
+print("나누면 \(calculator2.divide())")
 //calculator 라고만 되어있어서 답이 이상하게 출력됐던것! 가리키는 대상이 옳게 연결되어있는지 잘 확인하자
-print(calculator2.remaind())
+print("나머지는 \(calculator2.remaind())")
+*/
 
+
+class Calculator {
+    
+    let number1: Int
+    let number2: Int
+    
+    init(n1: Int, n2: Int) {
+        self.number1 = n1
+        self.number2 = n2
+    }
+    
+    func calculate(){
+                print("덧셈 결과: \(AddOperation(n1: number1, n2: number2).add())")
+                print("뺄셈 결과: \(SubtractOperation(n1: number1, n2: number2).subtract())")
+                print("곱셈 결과: \(MultiplyOperation(n1: number1, n2: number2).multiply())")
+                print("나눗셈 결과: \(DivideOperation(n1: number1, n2: number2).divide())")
+    }
+}
+
+class AddOperation {
+    var x: Int
+    var y: Int
+    
+    init(n1: Int, n2: Int) {
+        self.x = n1
+        self.y = n2
+    }
+    
+    func add() -> Int{
+        return x + y
+    }
+}
+
+class SubtractOperation {
+    var x: Int
+    var y: Int
+    
+    init(n1: Int, n2: Int) {
+        self.x = n1
+        self.y = n2
+    }
+    
+    func subtract() -> Int{
+        return x - y
+    }
+}
+
+class MultiplyOperation {
+    var x: Int
+    var y: Int
+    
+    init(n1: Int, n2: Int) {
+        self.x = n1
+        self.y = n2
+    }
+    
+    func multiply() -> Int{
+        return x * y
+    }
+}
+
+class DivideOperation {
+    var x: Int
+    var y: Int
+    
+    init(n1: Int, n2: Int) {
+        self.x = n1
+        self.y = n2
+    }
+    
+    func divide() -> Int{
+        return x / y
+    }
+}
+
+
+let answer = Calculator(n1: 40, n2: 4)
+answer.calculate()
 
