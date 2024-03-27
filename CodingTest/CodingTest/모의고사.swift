7b1ae162450d3bad2e73a5a6a0289211705d5660
DDattj <20191886@sungshin.ac.kr>
2024-03-27 10:02:08 +0900

    코딩테스트 모의고사

--- CodingTest/CodingTest/모의고사.swift
+++ CodingTest/CodingTest/모의고사.swift
@@ -15,41 +15,30 @@ func solution(_ answers:[Int]) -> [Int] {
     
     //답과 일치하는 경우만 걸러낸 후 그 개수를 각 학생들에게 부여
     //비교를 해서 더 큰쪽을 리턴, 개수가 동일하면 오름차순으로 리턴을 시키려고 했음
+    //하지만 이 이상으로 어떻게 풀어야 할지 몰라서 실패
 /*    let count1 = student1.filter { answers.contains($0) }.count
     let count2 = student2.filter { answers.contains($0) }.count
     let count3 = student3.filter { answers.contains($0) }.count
-    let ranking = [count1, count2, count3]
-    
-    
-    if count1 < count2 && count3 != 0 {
-        return [2]
-    } else if count2 < count1 && count3 != 0 {
-        return [1]
-    } else if count3 < count1 && count2 != 0 {
-        return [3]
-    } else if count1 == count2 || count2 == count3 || count1 == count3 {
-        return [raking.sorted(by: >)]
-    }
-    return result
-}*/
+*/
 
     // 각 수포자들의 정답 개수
        var scores = [0, 0, 0]
        
        // 정답 확인 및 점수 계산
        for (index, answer) in answers.enumerated() {
-           if answer == pattern1[index % pattern1.count] {
+           if answer == student1[index % student1.count] {
                scores[0] += 1
            }
-           if answer == pattern2[index % pattern2.count] {
+           if answer == student2[index % student2.count] {
                scores[1] += 1
            }
-           if answer == pattern3[index % pattern3.count] {
+           if answer == student3[index % student3.count] {
                scores[2] += 1
            }
        }
        
        // 가장 많은 문제를 맞힌 수포자(들) 찾기
+       //가장 많은 스코어를 얻은 사람을 리턴하도록, 만약 동점자가 발생한다면 인덱스가 작은것에서 큰것으로(오름차순으로) 정렬되도록 리턴
        let maxScore = scores.max() ?? 0
        var result: [Int] = []
        for (index, score) in scores.enumerated() {
