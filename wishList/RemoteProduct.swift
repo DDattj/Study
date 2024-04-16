8c97d5aae85bbe9e26bb5cc29ed9ac3e1b3e09aa
DDattj <157093311+DDattj@users.noreply.github.com>
2024-04-16 18:55:46 +0900

    Delete wishList directory

--- wishList/RemoteProduct.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RemoteProduct.swift
-//  wishList
-//
-//  Created by 이시안 on 4/16/24.
-//
-
-import Foundation
--- wishList/wishList/myWishList.swift
+++
@@ -1,8 +0,0 @@
-//
-//  myWishList.swift
-//  wishList
-//
-//  Created by 이시안 on 4/15/24.
-//
-
-import Foundation
